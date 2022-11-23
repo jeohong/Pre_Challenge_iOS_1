83b5d35565c803d0cf39a6897cd66a4c09248d34
jeohong <jeohong59@gmail.com>
2022-11-23 16:10:57 +0900

    ♻️ 오타 수정 성적상제 -> 성적삭제

--- MyCreditManager/main.swift
+++ MyCreditManager/main.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 while true {
-    print("원하는 기능을 입력해주세요\n1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적상제, 5: 평점보기, X: 종료")
+    print("원하는 기능을 입력해주세요\n1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적삭제, 5: 평점보기, X: 종료")
     let input = readLine()
     guard let input = input else { break }
     
