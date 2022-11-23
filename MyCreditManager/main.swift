f6e5a30d5efa7b9e7f5a845e7f12cc1c43454fd0
jeohong <jeohong59@gmail.com>
2022-11-23 16:08:23 +0900

    ✨ 입력에 따라 서로다른 결과 반환 ( 기본 틀 구성 )

--- MyCreditManager/main.swift
+++ MyCreditManager/main.swift
@@ -7,5 +7,26 @@
 
 import Foundation
 
-print("Hello, World!")
-
+while true {
+    print("원하는 기능을 입력해주세요\n1: 학생추가, 2: 학생삭제, 3: 성적추가(변경), 4: 성적상제, 5: 평점보기, X: 종료")
+    let input = readLine()
+    guard let input = input else { break }
+    
+    if input == "X" { print("프로그램을 종료합니다..."); break }
+    
+    switch input {
+    case "1":
+        print("학생추가 로직")
+    case "2":
+        print("학생삭제 로직")
+    case "3":
+        print("성적추가(변경) 로직")
+    case "4":
+        print("성적삭제 로직")
+    case "5":
+        print("평점보기 로직")
+    default:
+        print("뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요.")
+    }
+    
+}
