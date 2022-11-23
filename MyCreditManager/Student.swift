60644d91ceed3c6f017d7007063c1a37b8c51b14
jeohong <jeohong59@gmail.com>
2022-11-23 21:57:32 +0900

    ✨ Student 구조체 생성

---
+++ MyCreditManager/Student.swift
@@ -0,0 +1,13 @@
+//
+//  Student.swift
+//  MyCreditManager
+//
+//  Created by Hong jeongmin on 2022/11/23.
+//
+
+import Foundation
+
+struct Student {
+    let subject: [String] = []
+    let score: [String] = []
+}
